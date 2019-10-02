597e68fe9754d7ffd66def3b2a0f6860b787ca66
jewel789 <jewelalom.24csedu.002@gmail.com>
2019-10-02 19:35:31 +0600

    UI Design

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+   Date today = Calendar.getInstance().getTime();
+
+      TextView DateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       DateView=(TextView)findViewById(R.id.CurrentDate);
+
+       String date=today.toString();
+       String Day=date.substring(0,3);
+
+
+       DateView.setText(date.substring(11,20)+"\n"+date.substring(0,3)+" " +date.substring(8,10)+ " " +date.substring(4,8)+" "+date.substring(30,34));
+
+
     }
 }
